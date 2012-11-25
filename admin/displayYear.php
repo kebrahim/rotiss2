0ad99b48dc590ed2d9d55d8f0bc060331ceb7dee
Kurt Ebrahim <kurt.ebrahim@gmail.com>
2012-11-25 16:05:19 -0500

    ajaxify draft page; added internal border

---
+++ admin/displayYear.php
@@ -0,0 +1,67 @@
+<?php
+  require_once '../dao/ballDao.php';
+  require_once '../dao/draftPickDao.php';
+  require_once '../util/sessions.php';
+  require_once '../util/time.php';
+  
+  /**
+   * Displays draft results from specified year.
+   */
+  function displayDraftYear($year) {
+  	echo "<h1>Draft " . $year . "</h1><hr/>";
+  	
+  	// display table of draft picks for selected year, highlighting row for logged-in team
+  	$loggedInTeamId = SessionUtil::getLoggedInTeam()->getId();
+  	echo "<br/><br/><table border class='center'>
+  	        <th>Round</th><th>Pick</th><th>Team</th><th>Player</th></tr>";
+  	
+  	$pingPongBalls = BallDao::getPingPongBallsByYear($year);
+  	foreach ($pingPongBalls as $pingPongBall) {
+  	  echo "<tr";
+  	  if ($pingPongBall->getTeam()->getId() == $loggedInTeamId) {
+  		echo " class='selected_team_row'";
+  	  }
+  	  echo "><td>Ping Pong</td>
+  	         <td>" . $pingPongBall->getCost() . "</td>
+  		     <td>" . $pingPongBall->getTeam()->getNameLink(true) . "</td>
+  		     <td>" . displayPlayerLink($pingPongBall->getPlayer()) . "</td></tr>";
+  	  }
+  	
+  	  $draftPicks = DraftPickDao::getDraftPicksByYear($year);
+  	  foreach ($draftPicks as $draftPick) {
+  		echo "<tr";
+  		if ($draftPick->getTeam()->getId() == $loggedInTeamId) {
+  		  echo " class='selected_team_row'";
+  	    }
+  	    echo "><td>" . $draftPick->getRound() . "</td>
+  		       <td>" . $draftPick->getPick() . "</td>
+  	    	   <td>" . $draftPick->getTeam()->getNameLink(true) . "</td>
+  		       <td>" . displayPlayerLink($draftPick->getPlayer()) . "</td></tr>";
+  	  }
+    echo "</table>";
+  }
+  
+  function displayPlayerLink($player) {
+  	if ($player != null) {
+  	  return $player->getNameLink(true);
+  	} else {
+  	  return "--";
+  	}
+  }
+
+  // direct to corresponding function, depending on type of display
+  if (isset($_REQUEST["type"])) {
+  	$displayType = $_REQUEST["type"];
+  } else {
+  	die("<h1>Invalid display type for year</h1>");
+  }
+  if (isset($_REQUEST["year"])) {
+  	$year = $_REQUEST["year"];
+  } else {
+  	die("<h1>Invalid year param for year</h1>");
+  }
+
+  if ($displayType == "draft") {
+    displayDraftYear($year);
+  }
+?>
--- draftPage.php
+++ draftPage.php
@@ -8,76 +8,51 @@
 <title>Rotiss.com - Draft</title>
 <link href='css/style.css' rel='stylesheet' type='text/css'>
 </head>
+
+<script>
+// shows the draft page for the specified year
+function showYear(year) {
+    // If year is blank, then clear the yearDisplay div.
+	if (year=="" || year=="0") {
+		document.getElementById("yearDisplay").innerHTML="";
+		return;
+	}
+
+	// Display team information.
+	getRedirectHTML(document.getElementById("yearDisplay"),
+	    "admin/displayYear.php?type=draft&year=" + year);
+}
+
+// populates the innerHTML of the specified elementId with the HTML returned by the specified
+// htmlString
+function getRedirectHTML(element, htmlString) {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	} else {// code for IE6, IE5
+	    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			element.innerHTML=xmlhttp.responseText;
+		}
+	};
+	xmlhttp.open("GET", htmlString, true);
+	xmlhttp.send();
+}
+</script>
+
 <body>
 
 <?php
-  require_once 'dao/ballDao.php';
   require_once 'dao/draftPickDao.php';
   require_once 'util/navigation.php';
   require_once 'util/time.php';
 
-  function displayPlayerLink($player) {
-    if ($player != null) {
-      return $player->getNameLink(true);
-    } else {
-      return "--";
-    }
-  }
-
-  // display entire draft for specified year
-  function displayDraft($teamId, $year) {
-    echo "<div class='bodycenter'><h1>Draft " . $year . "</h1>";
-
-    // allow user to choose which year.
-    $minYear = DraftPickDao::getMinimumDraftYear();
-    $maxYear = DraftPickDao::getMaximumDraftYear();
-    echo "<strong>Filter by year: </strong>";
-    echo "<select name='year'>";
-    for ($yr = $minYear; $yr <= $maxYear; $yr++) {
-      echo "<option value='$yr'";
-      if ($yr == $year) {
-        echo " selected";
-      }
-      echo ">$yr</option>";
-    }
-    echo "</select>&nbsp&nbsp<input type='submit' name='submit' value='Filter'><br/><br/>";
-
-    // display table of draft picks for selected year, highlighting row for specified team
-    echo "<table border class='center'>
-          <th>Round</th><th>Pick</th><th>Team</th><th>Player</th></tr>";
-
-    $pingPongBalls = BallDao::getPingPongBallsByYear($year);
-    foreach ($pingPongBalls as $pingPongBall) {
-      echo "<tr";
-      if ($pingPongBall->getTeam()->getId() == $teamId) {
-        echo " class='selected_team_row'";
-      }
-      echo "><td>Ping Pong</td>
-             <td>" . $pingPongBall->getCost() . "</td>
-             <td>" . $pingPongBall->getTeam()->getNameLink(true) . "</td>
-             <td>" . displayPlayerLink($pingPongBall->getPlayer()) . "</td></tr>";
-    }
-
-    $draftPicks = DraftPickDao::getDraftPicksByYear($year);
-    foreach ($draftPicks as $draftPick) {
-      echo "<tr";
-      if ($draftPick->getTeam()->getId() == $teamId) {
-        echo " class='selected_team_row'";
-      }
-      echo "><td>" . $draftPick->getRound() . "</td>
-             <td>" . $draftPick->getPick() . "</td>
-             <td>" . $draftPick->getTeam()->getNameLink(true) . "</td>
-             <td>" . displayPlayerLink($draftPick->getPlayer()) . "</td></tr>";
-    }
-    echo "</table><br>";
-  }
-
   // Display header.
   NavigationUtil::printHeader(true, true, NavigationUtil::DRAFT_BUTTON);
-
-  // Use the logged-in user's team to highlight rows.
-  $teamId = SessionUtil::getLoggedInTeam()->getId();
-
+  echo "<div class='bodycenter'>";
+  
   // if year is not specified, use the year based on the end of the season.
   if (isset($_REQUEST["year"])) {
     $year = $_REQUEST["year"];
@@ -85,11 +60,29 @@
     $year = TimeUtil::getYearBasedOnEndOfSeason();
   }
 
-  // Display draft results.
-  echo "<form action='draftPage.php' method=post>";
-  displayDraft($teamId, $year);
-
-  echo "</form></div>";
+  // allow user to choose year.
+  $minYear = DraftPickDao::getMinimumDraftYear();
+  $maxYear = DraftPickDao::getMaximumDraftYear();
+  echo "<br/><label for='year'>Choose year: </label>";
+  echo "<select id='year' name='year' onchange='showYear(this.value)'>";
+  for ($yr = $minYear; $yr <= $maxYear; $yr++) {
+  	echo "<option value='" . $yr . "'";
+  	if ($yr == $year) {
+  		echo " selected";
+  	}
+  	echo ">$yr</option>";
+  }
+  echo "</select>";
+  echo "<div id='yearDisplay'></div><br/>";
+?>
+    
+<script>
+  // initialize yearDisplay with selected year
+  showYear(document.getElementById("year").value);
+</script>
+    
+<?php
+  echo "</div>";
 
   // Display footer
   NavigationUtil::printFooter();
