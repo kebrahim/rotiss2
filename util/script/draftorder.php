d2d7e35b596f27b7d793dd25adc849eb06b3a7c7
Kurt Ebrahim <kurt.ebrahim@gmail.com>
2013-02-02 11:03:19 -0500

    draft order script reads in from file

--- util/script/draftorder.php
+++ util/script/draftorder.php
@@ -2,14 +2,25 @@
   require_once 'commonScript.php';
   CommonScript::requireFileIn('/../../dao/', 'draftPickDao.php');
 
-  $teams = TeamDao::getAllTeams();
-  $draftYear = 2013;
-  echo "<h1>Generating $draftYear Draft...</h1>";
-  for ($round = 1; $round <= 20; $round++) {
-    foreach ($teams as $team) {
-      $draftPick = new DraftPick(-1, $team->getId(), $draftYear, $round, null, null, null);
-      //$draftPick = DraftPickDao::createDraftPick($draftPick);
-      echo $draftPick->toString() . " - " . $draftPick->getTeam()->getAbbreviation() . "<br/>";
-    }
+  // read in draft order from file
+  $fh = fopen("Draft_Order_2013.csv", 'r');
+  $header = fgetcsv($fh);
+
+  $data = array();
+  while ($line = fgetcsv($fh)) {
+    $data[] = array_combine($header, $line);
+  }
+  fclose($fh);
+
+  foreach ($data as $line) {
+    print_r($line);
+    echo "<br/>";
+    $orig = ($line["originalteam"] == "") ? null : $line["originalteam"];
+    $draftPick = new DraftPick(-1, $line["team"], 2013, $line["round"], $line["pick"], $orig, null);
+    //$draftPick = DraftPickDao::createDraftPick($draftPick);
+    echo $draftPick->toString() . " - " . $draftPick->getTeam()->getAbbreviation() .
+       (($draftPick->getOriginalTeam() == null) ? "" :
+          (" (" . $draftPick->getOriginalTeam()->getAbbreviation() . ")")) .
+    "<br/><br/>";
   }
 ?>
