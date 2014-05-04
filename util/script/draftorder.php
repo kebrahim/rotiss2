9b99fba282229e8877334fae5761dc8aa7a8b45d
Kurt Ebrahim <kurt.ebrahim@gmail.com>
2014-05-04 10:24:18 -0400

    Updated draft order script for 2016

--- util/script/draftorder.php
+++ util/script/draftorder.php
@@ -2,52 +2,54 @@
   require_once 'commonScript.php';
   CommonScript::requireFileIn('/../../dao/', 'draftPickDao.php');
 
+  // TODO user inputs year & file
+  $year = 2016;
+  $filename = "Draft_Order_2016.csv";
   $saveData = false;
 
   // read in draft order from file
-  $fh = fopen("Draft_Order_2015.csv", 'r');
+  $fh = fopen($filename, 'r');
   $header = fgetcsv($fh);
-
   $data = array();
   while ($line = fgetcsv($fh)) {
     $data[] = array_combine($header, $line);
   }
   fclose($fh);
 
-  // Rounds 1-2
+  // Rounds 1-4
   foreach ($data as $line) {
     //print_r($line);
-    $draftPick = new DraftPick(-1, $line["team"], 2015, 1, $line["pick"], null, null,
+    $draftPick = new DraftPick(-1, $line["team"], $year, $line["round"], $line["pick"], null, null,
         false);
     if ($saveData) {
       $draftPick = DraftPickDao::createDraftPick($draftPick);
     }
     echo $draftPick->toString() . " - " . $draftPick->getTeam()->getAbbreviation() . "<br/>";
 
-    $draftPickRev = new DraftPick(-1, $line["team"], 2015, 2, (17 - $line["pick"]), null, null,
-        false);
+    $draftPickRev = new DraftPick(-1, $line["team"], $year, $line["round"] + 1,
+        (17 - $line["pick"]), null, null, false);
     if ($saveData) {
       $draftPickRev = DraftPickDao::createDraftPick($draftPickRev);
     }
     echo $draftPickRev->toString() . " - " . $draftPickRev->getTeam()->getAbbreviation() . "<br/>";
   }
 
-  // Rounds 3-20
-  for ($round = 3; $round < 20; $round += 2) {
+  // Rounds 5-20
+  for ($round = 5; $round < 20; $round += 2) {
     // randomize team IDs
     $numbers = range(1,16);
     shuffle($numbers);
 
     $pick = 1;
     foreach ($numbers as $number) {
-      $draftPick = new DraftPick(-1, $number, 2015, $round, $pick, null, null,
+      $draftPick = new DraftPick(-1, $number, $year, $round, $pick, null, null,
           false);
       if ($saveData) {
         $draftPick = DraftPickDao::createDraftPick($draftPick);
       }
       echo $draftPick->toString() . " - " . $draftPick->getTeam()->getAbbreviation() . "<br/>";
 
-      $draftPickRev = new DraftPick(-1, $number, 2015, $round+1, (17 - $pick++), null, null,
+      $draftPickRev = new DraftPick(-1, $number, $year, $round + 1, (17 - $pick++), null, null,
           false);
       if ($saveData) {
         $draftPickRev = DraftPickDao::createDraftPick($draftPickRev);
